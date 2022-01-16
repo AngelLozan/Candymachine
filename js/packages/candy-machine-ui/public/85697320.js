20e37a881a59ebe3bd2aa2558c76308475c999ff
Scott Lozano <lozans89@gmail.com>
2022-01-16 16:03:16 +0000

    login

--- js/packages/candy-machine-ui/public/85697320.js
+++ js/packages/candy-machine-ui/public/85697320.js
@@ -5,4 +5,4 @@
 	password=prompt('Enter Password',' '); 
 	if (password==pass1) 
 	alert('Time to mint some SOL NFTs, make sure you are connected to your Exodus-Phantom Wallet and mainnet-beta. Please close this browser window when done. Click "OK" to proceed');
-	else { window.location="http://localhost:3000"; };
+	else { window.location="https://scottlozano.github.io/Candymachine/"; };
